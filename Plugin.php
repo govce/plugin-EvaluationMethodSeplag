ce939d7e2c8de5944abdb015bb42c6807314eeba
João Vitor Barbosa <contato.cavalcantebarbosa@gmail.com>
2021-03-01 15:21:49 -0300

    init plugin

---
+++ Plugin.php
@@ -0,0 +1,20 @@
+<?php 
+
+namespace EvaluationMethodSeplag;
+
+use MapasCulturais\App;
+
+class Plugin extends \MapasCulturais\Plugin {
+  public function __construct(array $config = [])
+  {
+      parent::__construct($config);
+  }
+  
+  function _init() {
+
+  }
+
+  function _register() {
+
+  }
+}
